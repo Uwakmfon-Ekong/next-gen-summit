406d5c82aaaf73a0bb130add70069d04cd509857
Uwakmfon-Ekong <uwakmfone470@gmail@example.com>
2026-07-21 02:47:54 +0100

    Update X link in footer to point to the correct URL

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="mt-4 flex flex-wrap justify-center gap-5 text-[13px]">
           <a className="border-b border-stone-700 pb-0.5 no-underline hover:border-[#d94a2b]" href="mailto:whakee@nextgensummit.xyz">Email</a>
           <a className="border-b border-stone-700 pb-0.5 no-underline hover:border-[#d94a2b]" href="https://t.me/whakeee" target="_blank" rel="noreferrer">Telegram</a>
-          <a className="border-b border-stone-700 pb-0.5 no-underline hover:border-[#d94a2b]" href="https://x.com/whakee_" target="_blank" rel="noreferrer">X</a>
+          <a className="border-b border-stone-700 pb-0.5 no-underline hover:border-[#d94a2b]" href="https://x.com/nextgensummit26?s=20" target="_blank" rel="noreferrer">X</a>
         </div>
       </div>
     </footer>
